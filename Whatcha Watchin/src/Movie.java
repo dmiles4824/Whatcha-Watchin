e7deedf0ba775987522a146fe258a33581e64384
Kaius Reed <kaikai11@live.com>
2016-02-13 00:24:49 -0500

    Updated User class

--- Whatcha Watchin/src/Movie.java
+++ Whatcha Watchin/src/Movie.java
@@ -2,9 +2,15 @@
 public class Movie 
 {
 	private String title;
+	private String yearOfRelease;
 	
 	public Movie(String title)
 	{
 		this.title = title;
+	}
+	
+	public String getTitle()
+	{
+		return title;
 	}
 }
--- Whatcha Watchin/src/User.java
+++ Whatcha Watchin/src/User.java
@@ -2,11 +2,40 @@ import java.util.Hashtable;
 
 public class User 
 {
+	//user's name
 	private String username;
+	
+	//list to contain user's personal list of movies
 	private Hashtable<Movie,Boolean> personalList;
 	
 	public User(String username)
 	{
 		this.username = username;
 	}
+	
+	//Get the username
+	public String getUserName()
+	{
+		return username;
+	}
+	
+	//Get the user's list of movies
+	public Hashtable<Movie,Boolean> getList()
+	{
+		return personalList;
+	}
+	
+	//Adds a new movie to the user's list
+	public void addMovie(Movie newMovie, Boolean liked)
+	{
+		personalList.put(newMovie,liked);
+	}
+	
+	//Remove a movie from the user's list
+	public void removeMovie(Movie selectedMovie)
+	{
+		personalList.remove(selectedMovie);
+	}
+	
+	
 }
