52b9041edd096d84469ba2cb1ff256cd434ebee1
Danny Miles <acommonsandvich@gmail.com>
2016-02-13 00:55:03 -0500

    Query Skeleton
    Theoretically, this should produce something when connected to the
    database. For testing purposes.

--- src/DatabaseQuerier.java
+++ src/DatabaseQuerier.java
@@ -5,22 +5,34 @@ import java.sql.*;
 
 public class DatabaseQuerier {
 		
-	//DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver();
-	
-
-
-	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub	
+	
 		try {	
 			String host = "";
 			String uName = "Your_Username";
 			String pass = "Your_Pass";
 			Connection con = DriverManager.getConnection( "host", "username", "password" );
+			
+			Statement stmt = conn.createStatement();
+			
+			String srtSelect = "Select username from user";
+			System.out.println("The SQL query is: " + strSelect); //Echo for debugging :D
+			System.out.println();
+			
+			ResultSet rset = stmt.executeQuery(strSelect);
+			
+			System.out.println("The usernames selected are:");
+			int rowCount = 0;
+			while (rset.next()) {
+				String username = rset.getString("username");
+				System.out.println(username);
+				++rowCount;
+			}
+			System.out.println("Total number of users = " + rowCount);
 			}
 			catch (SQLException e) {
-				;
+				e.printStackTrace();
 			}
 	}
 }
