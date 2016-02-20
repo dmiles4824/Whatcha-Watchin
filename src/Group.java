682c9740c85d8908d7bba3ec8e4c1e52cab0dfbd
Danny Miles <dmiles4824@msn.com>
2016-02-19 22:29:20 -0500

    Eliminated my extra local Whatcha Watchin folder

--- src/Group.java
+++ src/Group.java
@@ -1,45 +1,49 @@
-import java.util.Hashtable;
-import java.util.ArrayList<E>
+import java.util.*;
 
 public class Group{
   private String groupName;
-  private String[] members;//List of string names of members
-  private String admin;//name of admin
-  private ArrayList<Integer> groupList;//List of MovieIds
+  private User[] members;
+  private User admin;
+  private ArrayList<Movie> groupList;
+  private Hashtable<Movie,Integer> groupTable;
+  private int groupID;
   
-  public Group(String groupLeader){
+  public Group(User groupLeader){
     this.addMember(groupLeader);
     //make groupLeader the admin
   }
   
-  public void addMember(String newMember){
+  public void addMember(User newMember){
 	  //Danny function for adding a member to a group
   }
   
   //returns the list of members in the group
-  public String[] getMembers(){
-	return //Danny code  
+  public User[] getMembers(){
+	return null; //Danny code  
   }
   
-  //Returns list of MovieIds
-  public ArrayList<Movie> getGroupList(){ 
-	String[] members = getMembers();
-	groupTable = new Hashtable<Integer,Integer>();//<MovieID,NumberOfWants>
-	for(i=0;i<members.length;i++){
-		ArrayList<Movie> memberList = members[i].getList();//personal list of a single member
-		for(j=0;j<memberList.length;j++){
-			if(groupTable.containsKey(memberList.get(j))){
-				groupTable.put(memberList.get(j),groupList.get(memberList.get(j)+1));
+  public ArrayList<Movie> getGroupList(){ //kauhrgiaer=
+	members = getMembers();
+	groupTable = new Hashtable<Movie,Integer>();
+	for(int i=0;i<members.length;i++)
+	{
+		ArrayList<Movie> memberList = members[i].getPersonalList();
+		for(int j=0;j<memberList.size();j++){
+			if(groupTable.containsKey(memberList.get(j)))
+			{
+				groupTable.put(memberList.get(j),groupList.get(memberList[j])+1);
 			}
-			else{
-				groupTable.put(memberList.get(j),1);
+			else
+			{
+				groupTable.put(memberList[j],1);
 			}	
 		}	
 	}
-	groupList = new ArrayList<Integer>();//<MovieID>
-	set<Integer> movieList = groupTable.keySet();
-	Iterator<Integer> movieListIterator = movieList.iterator();//<MovieID>
-	while(movieListIterator.hasNext()){ //Insertion Sort
+	groupList = new ArrayList<Movie>();
+	Set<Movie> movieList = groupTable.keySet();
+	Iterator<Movie> movieListIterator = movieList.iterator();
+	while(movieListIterator.hasNext())
+	{
 		Movie currentMovie = movieListIterator.next();
 		i=0;
 		while(groupList.get(i)!=null || groupTable.get(groupList.get(i))<groupTable.get(currentMovie)){
@@ -50,14 +54,21 @@ public class Group{
 	return groupList;
   }
   
-  public removeUser(String Noah){
+  public int getID()
+  {
+	  return groupID;
+  }
+  
+  public void removeUser(User Noah){
 	  //
   }
   
-  public watchedMovie(int movieID){
-	String[] members = getMembers()  
-	for(i=0;i<members.length;i++){
-		members[i].watchedMovie(movieID);
+  public void matchedMovie(Movie movie)
+  {
+	members = getMembers();  
+	for(int i=0;i<members.length;i++)
+	{
+		members[i].watchedMovie(movie);
 	}
   }
   
