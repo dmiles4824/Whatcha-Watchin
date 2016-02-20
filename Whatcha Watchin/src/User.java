286b6573203d1eb3d0b3a0e17a5e955e941e3f4f
Kaius Reed <kaikai11@live.com>
2016-02-19 22:01:38 -0500

    Changed stored type of ArrayLists in User

--- Whatcha Watchin/src/User.java
+++ Whatcha Watchin/src/User.java
@@ -15,108 +15,144 @@ public class User
 	private String email;
 	
 	//list to contain user's personal list of movies
-	private ArrayList<Movie> personalList;
+	private ArrayList<Integer> personalList;
 	
 	//list of the groups that the user is in
-	private ArrayList<Group> groupList;
+	private ArrayList<Integer> groupList;
 	
 	//list of watched movies
-	private ArrayList<Movie> watchedList;
+	private ArrayList<Integer> watchedList;
 	
 	public User(String username, String password, String email)
 	{
 		this.username = username;
 		this.password = password;
 		this.email = email;
-		this.personalList = new ArrayList<Movie>();
-		this.watchedList = new ArrayList<Movie>();
-		this.groupList = new ArrayList<Group>();
+		this.personalList = new ArrayList<Integer>();
+		this.watchedList = new ArrayList<Integer>();
+		this.groupList = new ArrayList<Integer>();
 	}
 	
-	//Get the username
+	/**
+	 * Get the username
+	 * @return String containing username
+	 */
 	public String getUserName()
 	{
 		return username;
 	}
 	
-	//Get the user's password
+	/**
+	 * Get the user's password
+	 * @return String containing the user's password
+	 */
 	public String getPassword()
 	{
 		return password;
 	}
 	
-	//Get the user's email
+	/**
+	 * Get the user's email
+	 * @return String containing the user's email address
+	 */
 	public String getEmail()
 	{
 		return email;
 	}
 	
-	//Get the user's personal list of movies
-	public ArrayList<Movie> getPersonalList()
+	/**
+	 * Get the user's personal list of movies
+	 * @return An ArrayList<Integer> of movie IDs that are in the user's personal list
+	 */
+	public ArrayList<Integer> getPersonalList()
 	{
 		return personalList;
 	}
 	
-	//Get the user's list of already watched movies
-	public ArrayList<Movie> getWatchedList()
+	/**
+	 * Get the user's list of already watched movies
+	 * @return An ArrayList<Integer> of movie IDs of movies 
+	 * that the user has already watched
+	 */
+	public ArrayList<Integer> getWatchedList()
 	{
 		return watchedList;
 	}
 	
-	//Get the list of groups that the user belongs to
-	public ArrayList<Group> getGroups()
+	/**
+	 * Get the list of groups that the user belongs to
+	 * @return An ArrayList<Integer> of group IDs of groups that the user belongs to
+	 */
+	public ArrayList<Integer> getGroups()
 	{
 		return groupList;
 	}
 	
-	//Adds a new movie to the user's list
-	public void addMovie(ArrayList<Movie> chosenList, Movie chosenMovie)
+	/**
+	 * Adds a new movie to the user's list
+	 * @param chosenList List to add the movie to (Personal or Watched)
+	 * @param movieID ID of the movie that is being added
+	 */
+	public void addMovie(ArrayList<Integer> chosenList, int movieID)
 	{
 		//****Do something with database to update when adding a movie
-		chosenList.add(chosenMovie);
+		chosenList.add(movieID);
 	}
 	
-	//Remove a movie from the user's list
-	public void removeMovie(ArrayList<Movie> chosenList, Movie selectedMovie)
+	/**
+	 * Remove a movie from the user's list
+	 * @param chosenList List to remove a movie from (Personal or Watched)
+	 * @param selectedMovieID ID of the movie to be removed
+	 */
+	public void removeMovie(ArrayList<Integer> chosenList, int selectedMovieID)
 	{
-		personalList.remove(selectedMovie);
+		chosenList.remove(selectedMovieID);
 	}
 	
-	//Adds a movie to watched list and removes it from personal list
-	public void watchedMovie(Movie selectedMovie)
+	/**
+	 * Adds a movie to watched list and removes it from personal list
+	 * @param selectedMovieID ID of the movie to be denoted as watched
+	 */
+	public void watchedMovie(int selectedMovieID)
 	{
-		String name = selectedMovie.getTitle();
 		int index = 0;
 		
 		//Check through movie list for the movie we want to put in the watch list
-		while(getWatchedList().get(index).getTitle() != name || index < getWatchedList().size())
+		while(getWatchedList().get(index)!= selectedMovieID || index < getWatchedList().size())
 			index++;
 		
 		//Only perform operations if we managed to find our movie/didn't go out of array bound
 		if(getPersonalList().get(index) != null)
 		{
 			//*****UPDATE DATABASE INFO
-			addMovie(getWatchedList(), selectedMovie);
-			removeMovie(getPersonalList(), selectedMovie);
+			addMovie(getWatchedList(), selectedMovieID);
+			removeMovie(getPersonalList(), selectedMovieID);
 		}
 		else
 			System.out.println("Movie is not in list");
 	}
 	
-	//Remove a user from a selected group (Only works if user is the admin of the group)
-	public void removeMember(User selectedUser, Group selectedGroup)
+	/**
+	 * Remove a user from a selected group (Only works if user is the admin of the group)
+	 * @param selectedUsername Username of the user to be removed from the group
+	 * @param selectedGroupID ID of the group to remove user from
+	 */
+	public void removeMember(String selectedUsername, int selectedGroupID)
 	{
 		//****UPDATE GROUP MEMBER LIST IN DATABASE
 	}
 	
-	//Remove this current user from one of the selected groups that they are in
-	public void leaveGroup(Group selectedGroup)
+	/**
+	 * Remove this current user from one of the selected groups that they are in
+	 * @param selectedGroup ID of the group to leave
+	 */
+	public void leaveGroup(int selectedGroupID)
 	{
 		//*****UPDATE GROUP MEMBER LIST IN DATABASE;
 	}
 	
 	//Make a random movie suggestion for a group from its list of movies
-	public void suggest(Group selectedGroup)
+public void suggest(int groupID)
 	{
 		int random;
 		
@@ -126,15 +162,15 @@ public class User
 	public void suggestSelf()
 	{
 		int random = (int)(Math.random() * (this.getPersonalList().size()));
-		System.out.println("Your suggested movie is.....\n" + getPersonalList().get(random).getMovieInfo());
+		System.out.println("Your suggested movie is.....\n" + getPersonalList().get(random));
 	}
 	
 	public static void main(String[] args)
 	{
 		User kai = new User("Kaius", "Blah", "kek");
-		Movie movie1 = new Movie("The First Movie");
-		Movie movie2 = new Movie("The Second Movie");
-		Movie movie3 = new Movie("The Third Movie");
+		int movie1 = 951291;
+		int movie2 = 12839212;
+		int movie3 = 5421029;
 		
 		kai.addMovie(kai.getPersonalList(), movie1);
 		kai.addMovie(kai.getPersonalList(), movie2);
